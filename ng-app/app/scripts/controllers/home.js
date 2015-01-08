abfe56d31d6dfe90672e69e7ba06c9e83707898b
bpwalford <bpwalford@gmail.com>
2015-01-08 12:05:00 -0700

    got basic board working with continual updating

--- ng-app/app/scripts/controllers/home.js
+++ ng-app/app/scripts/controllers/home.js
@@ -1,11 +1,48 @@
 'use strict';
 
 angular.module('benwalfordApp')
-  .controller('HomeCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('HomeCtrl', function ($scope, $interval) {
+
+    var Cell = function (state) {
+      this.state = state;
+
+      this.toggle = function() {
+        if (this.state === 'dead') {
+          this.state = 'alive';
+        } else {
+          this.state = 'dead';
+        }
+      };
+    };
+
+    $scope.cells = [
+      [ (new Cell('dead')),  (new Cell('alive')),  (new Cell('dead')),  ],
+      [ (new Cell('alive')), (new Cell('dead')), (new Cell('alive')), ],
+      [ (new Cell('dead')),  (new Cell('alive')),  (new Cell('dead')),  ],
     ];
-    $scope.nums = [1,2,3];
+
+    $interval(function(){
+      for (var i = 0; i < $scope.cells.length; i++) {
+        for (var j = 0; j < $scope.cells[i].length; j++) {
+          $scope.cells[i][j].toggle();
+        }
+      }
+    }, 1000);
+
+  })
+  .directive('changeState', function(){
+    return {
+      scope: false,
+      templateUrl: '../../views/board.html'
+    };
   });
+
+
+
+
+
+
+
+
+
+  // end
