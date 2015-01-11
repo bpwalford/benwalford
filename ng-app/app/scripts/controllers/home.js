54bc047b0ecddcb71ffc7e930d7076a067b5c58d
bpwalford <bpwalford@gmail.com>
2015-01-11 16:16:10 -0700

    game of life working

--- ng-app/app/scripts/controllers/home.js
+++ ng-app/app/scripts/controllers/home.js
@@ -6,15 +6,11 @@ angular.module('benwalfordApp')
     $scope.cells = World.initialState;
 
     function getCellState(x,y){
-      var state;
-      // console.log('y: ' + y + ' | x: ' + x );
-      try {
-        state = $scope.cells[y][x].state;
-      } catch(err) {
-        state = undefined;
+      if ($scope.cells[y] && $scope.cells[y][x]) {
+        return $scope.cells[y][x].state;
+      } else {
+        return World.dead;
       }
-
-      return state;
     }
 
 
@@ -35,8 +31,7 @@ angular.module('benwalfordApp')
           livingNeighbors++;
         }
       }
-      // console.log(livingNeighbors);
-      // console.log('*********************');
+
       return livingNeighbors;
     }
 
@@ -50,6 +45,8 @@ angular.module('benwalfordApp')
       } else {
         if (neighbors === 3) {
           return $scope.cells[y][x].toggle();
+        } else {
+          return $scope.cells[y][x].state;
         }
       }
     }
@@ -60,17 +57,15 @@ angular.module('benwalfordApp')
       for (var y = 0; y < $scope.cells.length; y++) {
         for (var x = 0; x < $scope.cells[y].length; x++) {
           var neighbors = countLivingNeighbors(x,y);
-          console.log('x: ' + x + ' | y: ' + y + ' | state: ' + $scope.cells[y][x].state + ' | neighbors: ' + neighbors);
 
-          var state = calculateNextCellState(x,y,neighbors) || World.dead;
+          var state = calculateNextCellState(x,y,neighbors);
 
           nextState[y][x] = new World.buildCell(state);
         }
       }
 
       $scope.cells = nextState;
-      console.log('*********************');
-    }, 1);
+    }, 50);
 
   }])
   .directive('changeState', function(){
@@ -79,3 +74,36 @@ angular.module('benwalfordApp')
       templateUrl: '../../views/board.html'
     };
   });
+  // .directive('theGame', function() {
+  //   return {
+  //     scope: {
+  //       "currentWorld": "="
+  //     },
+  //     link: function(scope, elem) {
+  //       function renderTheWorld() {
+  //         // Do world rendering and operate on elem.
+  //       };
+  //
+  //       scope.$watch("currentWorld", renderTheWorld)
+  //     };
+  //   }
+  // });
+  // .directive('tickWorld', function() {
+  //   return {
+  //     scope: {
+  //       "initialWorld": "="
+  //     },
+  //     link: function(scope) {
+  //       scope.currentWorld = scope.initialWorld;
+  //
+  //       tickPromise = $interval(function() {
+  //         scope.world = scope.world.tick();
+  //       }, 200);
+  //
+  //       $scope.on("$destroy", tickPromise.cancel);
+  //     }
+  //   }
+  // })
+  // <tick-world initial-world="initialWorld">
+  //   <the-game world="currentWorld"></the-game>
+  // </tick-world>
